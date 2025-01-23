badf92a481cbaa71416ddb6d8d30c0d89fd7dbae
Elliot <dge_1992@163.com>
2025-01-23 16:20:56 +0800

    add something about aop and vue

--- web-project/hello_vue3/src/components/LoveTalk.vue
+++ web-project/hello_vue3/src/components/LoveTalk.vue
@@ -1,24 +1,27 @@
 <template>
-    <div class="count">
-        
+    <div class="talk">
+        <button>获取一句土味情话</button>
     </div>
+    <ul>
+        <li v-for="talk in talkList" :key="talk.id">{{ talk.title }}</li>
+    </ul>
 </template>
 
-<script setup lang="ts" name="Count">
+<script setup lang="ts">
+    import { reactive } from 'vue';
 
+    let talkList = reactive([
+        {id:'dsads1', title: "哈哈"},
+        {id:'dsads2', title: "呵呵"},
+        {id:'dsads3', title: "嘻嘻"}
+    ])
 </script>
 
-<style scoped>
-    .count{
-        background-color: skyblue;
+<style>
+    .talk{
+        background-color: orange;
         box-shadow: 0px 0px 10px;
         border-radius: 10px;
         padding: 10px;
     }
 </style>
-
-
-
-
-
-
